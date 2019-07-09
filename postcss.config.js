322154da30691b93022ce3ec40e31ea5afbc99f0
Anthony Mittaz <sync@me.com>
2019-07-10 09:32:57 +1000

    We no longer need this override

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,3 @@
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
-  plugins: [
-    autoprefixer({
-      overrideBrowserslist: ['> 1%', 'last 4 versions'],
-    }),
-  ],
+  plugins: [require('autoprefixer')],
 };
